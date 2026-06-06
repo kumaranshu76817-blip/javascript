eb968ee1b64ddcdfc28bca54a3d8520f2011b172
Anshu <kumaranshu76817@gmail.com>
2026-06-06 14:36:38 +0530

    add datat type and converting

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all js code as newer version
+
+// alert( 3 +3) // we are using nodejs, not browser
+
+console.log(3 + 3)//code readability should be high
+console.log();
+
+let name = "anshu"
+let age = 18
+let isLoggedIn = false
+let state = null
+
+// number => 2 to power 53
+// bigInt
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+// object
+
+console.log(typeof"anshu")// string
+console.log(typeof null); // object
---
+++ 01_basics/03_convertionOperation.js
@@ -0,0 +1,72 @@
+let score = "33"
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumbr = Number(score)
+console.log(typeof valueInNumbr)
+console.log(valueInNumbr);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+
+let isLoggedIn  = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+
+//1 => tru; 0 =>false
+//"" => false
+//"anshu" => true
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber)
+
+
+// 33 => 33
+
+
+// -------------------operations--------------
+
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+
+console.log(2 +2);
+console.log(2 -2);
+console.log(2 *2);
+console.log(2 **2);
+console.log(2 %2);
+console.log(2 /2);
+
+
+let str1 = "hello"
+let str2 = "anshu"
+let str3 = str1 + str2
+console.log(str3)
+
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1"+2+2)
+console.log(1 +2 +"2");
+
+console.log((3 +2) *5 %2);
+
+
+console.log(true);
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 +2
+console.log(num1);
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter);
+
